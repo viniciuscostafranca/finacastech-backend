939265f91b4f64e88237e998d41781c7deb805b9
Vinicius Costa franca <vinicius.franca@lello.com.br>
2024-08-26 16:56:31 -0300

    init

---
+++ src/main/java/com/financastech/fip/FipApplication.java
@@ -0,0 +1,13 @@
+package com.financastech.fip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financastech/fip/FipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financastech.fip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
